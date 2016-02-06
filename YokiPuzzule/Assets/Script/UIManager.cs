436f5f3a099ed479e542b83f4f0879054c31baa0
大澤 朝広 <tomohiro.ohsawa@gmail.com>
2016-02-07 00:06:59 +0900

    eventの実装2

--- YokiPuzzule/Assets/Script/UIManager.cs
+++ YokiPuzzule/Assets/Script/UIManager.cs
@@ -8,11 +8,6 @@ using System.Collections.Generic;
  */
 public class UIManager : MonoBehaviour {
 	/*	*
-	 * メソッドを直接コールするオブジェクト。
-	 */
-	private List<CallMethodTest> callMethodObjects = new List<CallMethodTest>();
-
-	/*	*
 	 * SendMessageイベントを呼ぶオブジェクト。
 	 */
 	private List<GameObject> sendMesssages = new List<GameObject>();
@@ -31,58 +26,10 @@ public class UIManager : MonoBehaviour {
 	 * OnGUI.
 	 */
 	public void OnGUI() {
-		if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height - 60, 100, 30), "Event")) {
-			int count = 0;
-			// foreach内での配列の変更はNGのためwhileで行う（イベント先で削除される可能性があるので）
-			// メソッドのコール
-			while (count < this.callMethodObjects.Count) {
-				CallMethodTest callMethodTest = this.callMethodObjects[count];
-				if (callMethodTest) callMethodTest.RotateCube();
-				else this.callMethodObjects.Remove(callMethodTest);
-				++count;
-			}
-
-			// SendMessage
-			count = 0;
-			while (count < this.sendMesssages.Count) {
-				GameObject go = this.sendMesssages[count];
-				if (go) go.SendMessage("RotateCube");
-				else this.sendMesssages.Remove(go);
-				++count;
-			}
-
-			// 登録メソッドの呼び出し
-			count = 0;
-			while (count < this.methods.Count) {
-				System.Action method = this.methods[count];
-				if (method != null) method();
-				else this.methods.Remove(method);
-				++count;
-			}
-
-			// イベント実行
-			if (this.OnClick != null) this.OnClick(this, EventArgs.Empty);
-
-		}
-
+		// イベント実行
+		if (this.OnClick != null) this.OnClick(this, EventArgs.Empty);
 	}
-
-	// ------------------------------
-
-	/*	*
-	 * 直接メソッドを呼び出すオブジェクトの追加。
-	 */
-	public void AddCallMethodObject(CallMethodTest callMethod) {
-		this.callMethodObjects.Add(callMethod);
-	}
-
-	/*	*
-	 * 直接メソッドを呼び出すオブジェクトの削除。
-	 */
-	public void RemoveCallMethodObject(CallMethodTest callMethod) {
-		this.callMethodObjects.Remove(callMethod);
-	}
-
+		
 	// ------------------------------
 
 	/*	*
